30d52e2154d26361f6af3a8385f96e4dd254a7ae
Stephen Leach <sfkleach@gmail.com>
2023-02-12 14:49:30 +0000

    Improve test coverage

--- tests/test_lazychains.py
+++ tests/test_lazychains.py
@@ -35,8 +35,66 @@ def test_nullary_construction():
     len_lc = len(lc)
     len_sc = len(sc)
     # Assert
+    assert not lc
+    assert not sc
     assert 0 == len_lc
     assert 0 == len_sc
+
+def test_nonnullary_construction():
+    # Arrange
+    lc = lazychain([99])
+    sc = chain([88])
+    # Assert
+    assert lc
+    assert sc
+
+def test_new():
+    # Arrange
+    lc0 = lazychain()
+    sc0 = chain()
+    # Act
+    lc1 = lc0.new(99)
+    sc1 = sc0.new(88)
+    # Assert
+    assert not lc0 and lc1
+    assert not sc0 and sc1
+    assert 1 == len(lc1)
+    assert 1 == len(sc1)
+    assert [99] == list(lc1)
+    assert [88] == list(sc1)
+
+def test_bool_is_expanded():
+    # Arrange
+    lc0 = lazychain("pqr")
+    lc1 = lazychain("pqr")
+    sc0 = chain("stu")
+    sc1 = chain("stu")
+    # Act
+    l1 = bool(lc1)
+    s1 = bool(sc1)
+    # Assert
+    assert l1 is True
+    assert s1 is True
+    assert not lc0.is_expanded()
+    assert lc1.is_expanded()
+    assert sc0.is_expanded()
+    assert sc1.is_expanded()
+
+def test_expanded_len():
+    # Arrange
+    sample = "abcdef"
+    c0 = lazychain( sample )
+    depth = 3
+    # Act
+    p = c0.expanded_len()
+    c0.len_is_at_least(depth)
+    q = c0.expanded_len()
+    c0.expand()
+    r = c0.expanded_len()
+    # Assert
+    assert 0 == p
+    assert depth == q
+    assert len(sample) == r
 
 def test_negative_indexes():
     # Arrange
@@ -58,31 +116,77 @@ def test_dest():
     # Assert
     assert x == "a"
     assert not y.is_expanded()
+    assert ["b", "c"] == list(y)
 
-def test_expanded_len():
+def test_head():
     # Arrange
     c = lazychain( "abc" )
     # Act
-    x, y = c.dest()
+    x = c.head()
     # Assert
     assert x == "a"
-    assert not y.is_expanded()    
+    assert c.is_expanded()
+    assert 1 == c.expanded_len()
+
+def test_tail():
+    # Arrange
+    c = lazychain( "abc" )
+    # Act
+    y = c.tail()
+    # Assert
+    assert not y.is_expanded()
+    assert 1 == c.expanded_len()
+    assert ["b", "c"] == list(y)  
 
 def test_map():
     # Arrange
-    sample = "abcdef"
-    c0 = lazychain( sample )
-    depth = 3
+    c = lazychain( "abcdef" )
     # Act
-    p = c0.expanded_len()
-    c0.len_is_at_least(depth)
-    q = c0.expanded_len()
-    c0.expand()
-    r = c0.expanded_len()
+    c = c.map(lambda x: f"-{x}-")
+    c0 = c[0]
+    c5 = c[5]
     # Assert
-    assert 0 == p
-    assert depth == q
-    assert len(sample) == r
+    assert "-a-" == c0
+    assert "-f-" == c5
+    assert 6 == len(c)
+
+def test_filter():
+    # Arrange
+    c = lazychain( "abcdef" )
+    # Act
+    x = list(c.filter(lambda x: x in "cat"))
+    # Assert
+    assert ["a", "c"] == x
+
+def test_zip():
+    # Arrange
+    c0 = lazychain( "ab" )
+    c1 = chain(range(0, 6))
+    # Act
+    x = list(c0.zip(c1))
+    # Assert
+    assert [("a", 0), ("b", 1)] == x
+
+def test_contains():
+    # Arrange
+    c = lazychain( "abxyz" )
+    # Assert
+    assert "a" in c
+    assert "g" not in c
+    assert "x" in c
+    assert "z" in c
+
+def test_add():
+    # Arrange
+    c0 = lazychain( "ab" )
+    c1 = chain(range(0, 6))
+    # Act
+    c_new = c0 + c1
+    x = list(c_new)
+    # Assert
+    assert ["a", "b", 0, 1, 2, 3, 4, 5] == x
+    assert c_new.tail().tail() is c1
+
 
 def test_lazycall():
     """
